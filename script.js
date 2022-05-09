0c5cc78d029f5bfca5a565869cd7f0e280e3f61a
ukadavid <ukadavid7@gmail.com>
2022-05-09 17:20:11 +0100

    Update on the features

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 'use strict';
 
-const number = Math.trunc(Math.random()* 20)  + 1;
-document.querySelector(".number").textContent = number;
+// document.querySelector(".again").addEventListener("click", function(){
+//     document.querySelector(".message").textContent = "Start guessing...";
+//     document.querySelector("body").style.backgroundColor = "#222";
+//     document.querySelector(".number").style.width = "15rem";
+//     document.querySelector(".score").textContent = number;
+// });
 
 let score = 20;
 
@@ -9,10 +13,21 @@ document.querySelector(".check").addEventListener('click', function(){
 
     const guess = Number(document.querySelector(".guess").value);
     console.log(guess)
+
+    // If no number was inputed, it would show "No number"
     if (!guess){
         document.querySelector(".message").textContent = "No number";
+
+        // If the number is guessed right, this else if statement will run
+        
     } else if (guess === number) {
+        const number = Math.trunc(Math.random()* 20)  + 1;
+        document.querySelector(".number").textContent = number;
         document.querySelector(".message").textContent = "Correct number";
+        document.querySelector("body").style.backgroundColor = "green";
+        document.querySelector(".number").style.width = "30rem";
+
+        // If the number guessed is greater than the number
     } else if (guess > number){
         if (score > 1){
         document.querySelector(".message").textContent = "Number too high";
